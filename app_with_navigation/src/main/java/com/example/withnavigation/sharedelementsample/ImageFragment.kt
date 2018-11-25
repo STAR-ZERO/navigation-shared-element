d0a37b61b8e167c3490acc49e70564292e080f04
Kenji Abe <kenji.01.star@gmail.com>
2018-11-25 15:34:01 +0900

    Improve animation

--- app_with_navigation/src/main/java/com/example/withnavigation/sharedelementsample/ImageFragment.kt
+++ app_with_navigation/src/main/java/com/example/withnavigation/sharedelementsample/ImageFragment.kt
@@ -1,12 +1,18 @@
 package com.example.withnavigation.sharedelementsample
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.withnavigation.sharedelementsample.databinding.FragmentImageBinding
+import com.example.withnavigation.sharedelementsample.util.GlideApp
 
 class ImageFragment : Fragment() {
 
@@ -24,9 +30,25 @@ class ImageFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        postponeEnterTransition()
 
         val imageURL = ImageFragmentArgs.fromBundle(arguments).imageURL
         binding.image.transitionName = imageURL
-        binding.imageURL = imageURL
+
+        GlideApp.with(this)
+                .load(imageURL)
+                .dontAnimate()
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                        startPostponedEnterTransition()
+                        return false
+                    }
+
+                    override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                        startPostponedEnterTransition()
+                        return false
+                    }
+                })
+                .into(binding.image)
     }
 }
--- app_without_navigation/src/main/java/com/example/withoutnavigation/sharedelementsample/ImageFragment.kt
+++ app_without_navigation/src/main/java/com/example/withoutnavigation/sharedelementsample/ImageFragment.kt
@@ -1,14 +1,20 @@
 package com.example.withoutnavigation.sharedelementsample
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.withoutnavigation.sharedelementsample.databinding.FragmentImageBinding
+import com.example.withoutnavigation.sharedelementsample.uitl.GlideApp
 
-class ImageFragment: Fragment() {
+class ImageFragment : Fragment() {
 
     private lateinit var binding: FragmentImageBinding
 
@@ -24,10 +30,26 @@ class ImageFragment: Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        postponeEnterTransition()
 
         arguments?.getString(KEY_IMAGE_URL).let { imageURL ->
             binding.image.transitionName = imageURL
-            binding.imageURL = imageURL
+
+            GlideApp.with(this)
+                    .load(imageURL)
+                    .dontAnimate()
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                            startPostponedEnterTransition()
+                            return false
+                        }
+
+                        override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                            startPostponedEnterTransition()
+                            return false
+                        }
+                    })
+                    .into(binding.image)
         }
     }
 
